719cbd7dbf8507ca24b4ddac4f7e2c4fc4be4368
Seth Thomas <sethmthomas89@gmail.com>
2026-08-22 13:40:43 -0500

    Add mock track data

---
+++ src/data/mockTracks.js
@@ -0,0 +1,17 @@
+export const mockTracks = [
+  {
+    title: "Midnight Drive",
+    album: "The Neon Hour",
+    artist: "Night Voltage",
+  },
+  {
+    title: "Static Bloom",
+    album: "Static Bloom",
+    artist: "Paper Satellites",
+  },
+  {
+    title: "A Very Long Song Title That Definitely Will Not Fit On One Line",
+    album: "Overflow Test",
+    artist: "The Marquee Testers",
+  },
+];
