186d43a38c8818beabe85414c71b535d534481e8
Seth Thomas <sethmthomas89@gmail.com>
2026-08-22 14:18:37 -0500

    Add TransportControls component

---
+++ src/components/TransportControls.jsx
@@ -0,0 +1,31 @@
+import { TransportKey } from "./TransportKey";
+import "./TransportControls.css";
+
+export function TransportControls({
+  status,
+  onPlay,
+  onPause,
+  onStop,
+  onSkipNext,
+  onSkipPrev,
+}) {
+  return (
+    <div className="transport-controls">
+      <TransportKey icon="◀◀" label="Skip back" onClick={onSkipPrev} />
+      <TransportKey
+        icon="▶"
+        label="Play"
+        onClick={onPlay}
+        latched={status === "playing"}
+      />
+      <TransportKey
+        icon="❙❙"
+        label="Pause"
+        onClick={onPause}
+        latched={status === "paused"}
+      />
+      <TransportKey icon="▶▶" label="Skip forward" onClick={onSkipNext} />
+      <TransportKey icon="■" label="Stop" onClick={onStop} />
+    </div>
+  );
+}
